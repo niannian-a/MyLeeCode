6e60fa775f3b0fba47ed01e8d2ad9b59d1c0853b
nian <1455398401@qq.com>
2026-03-13 15:42:03 +0800

    leecode1

--- www/leecode1.java
+++ www/leecode1.java
@@ -1,9 +1,6 @@
 package com.leecode.www;
 import java.util.Arrays;
-
-public class leecode1 {
-    /*给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
-
+/*给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
 你可以按任意顺序返回答案。
 示例 1：
@@ -22,6 +19,7 @@ public class leecode1 {
 -109 <= target <= 109
 只会存在一个有效答案
 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？*/
+public class leecode1 {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15,19,20,23,99};
         int target = 119;
